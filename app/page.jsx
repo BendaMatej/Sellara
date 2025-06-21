a0b52ff26f4f685d2915a3b1c60fc302d0f67071
BendaMatej <mshot.armada@gmail.com>
2025-06-21 18:15:12 +0200

    Delete app/page.jsx

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Vítej na Sellara</h1>
-      <p>Top produkty, nejlepší obchody. Začni prodávat ještě dnes.</p>
-    </main>
-  );
-}
