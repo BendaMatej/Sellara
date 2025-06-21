159bae458e488103dce7597f17ad2cb2aaa37e6c
BendaMatej <mshot.armada@gmail.com>
2025-06-21 17:08:58 +0200

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Vítej na Sellara</h1>
+      <p>Top produkty, nejlepší obchody. Začni prodávat ještě dnes.</p>
+    </main>
+  );
+}
